e64e7228194b6413c1ee39ae04b7b529da75d730
Alo OLuwapelumi <lumii.dev@gmail.com>
2023-08-24 16:27:38 +0100

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,54 +13,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 300,
-              width: 300,
-              color: Colors.amberAccent,
-            ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.pink
+                ),
+              ),
 
-            Container(
-              height: 200,
-              width: 200,
-              color: Colors.amberAccent[400],
-            ),
-
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.amberAccent[200],
-            ),
-          ],
-          child: Container(
-          height: 300,
-          width: 300,
-          decoration: BoxDecoration(
-              color: Colors.green,
-
-            borderRadius: BorderRadius.circular(20),
-          ),
-          padding: EdgeInsets.all(25),
-          child: Icon(
-            Icons.face,
-            color: Colors.white,
-            size: 64,
-          ),
-          ),
-
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight:
-            )
+              Expanded(
+                child: Container(
+                  color: Colors.blue
+                ),
+              ),
 
 
-          ),
+              Expanded(
+                child: Container(
+                  color: Colors.green
+                ),
+              ),
+            ],
           )
-        )
       ),
+
     );
   }
 }
